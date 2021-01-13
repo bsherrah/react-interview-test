03ec0cfa9f794371ee20cdd28c25d4b29e10ba46
Bsher <brahmoun@protonmail.com>
2021-01-13 19:31:00 +0300

    Update webpack plugin to let React use .env file

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: './client/src/index.html'
+  template: './client/src/index.html',
 });
+
+const env = dotenv.config().parsed;
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
+const envPlugin = new webpack.DefinePlugin(envKeys);
 
 module.exports = {
   entry: './client/src/index.js',
@@ -15,17 +24,15 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-react'],
-            plugins: [
-              ["@babel/plugin-proposal-decorators", { "legacy": true }],
-            ]
-          }
-        }
+            plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
+          },
+        },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlPlugin, envPlugin],
 };
