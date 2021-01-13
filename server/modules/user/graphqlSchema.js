84ee9fa92ae7f5bc571ae2a3709dd845621c6187
Bsher <brahmoun@protonmail.com>
2021-01-13 18:13:15 +0300

    Add graphql schemas and resolvers

---
+++ server/modules/user/graphqlSchema.js
@@ -0,0 +1,31 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type cartItem {
+    productId: String!
+    qty: Int!
+  }
+  type order {
+    orderId: String!
+    products: [cartItem!]!
+  }
+  type User {
+    name: String
+    email: String
+    picture: String
+    likedProducts: [String!]!
+    cartItems: [cartItem!]!
+    orders: [order!]!
+  }
+  type Query {
+    users: [User!]!
+  }
+  type Mutation {
+    login: User!
+    addToCart(productId: String!, qty: Int!): User!
+    resetCart: User!
+    likeProduct(productId: String!, liked: Boolean!): User!
+  }
+`;
+
+module.exports = typeDefs;
